029c73038e3342e7f975502b853ee0512d393fbb
Hasina37 <randriamarohasina@gmail.com>
2025-04-05 15:29:36 +0300

    Create footer.php
    ajout du fichier footer

---
+++ view/components/footer.php
@@ -0,0 +1,12 @@
+<!-- views/components/footer.php -->
+
+<footer class="bg-dark text-white text-center py-3 mt-5">
+  <div class="container">
+    <p class="mb-0">&copy; <?= date('Y') ?> Bibliothèque Universitaire. Tous droits réservés.</p>
+  </div>
+</footer>
+
+<link rel="stylesheet" href="../css/bootstrap.min.css">
+<script src="../js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
