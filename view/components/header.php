def0f1039fdaed5a5b2bc9045034c567dd39b17a
Hasina37 <randriamarohasina@gmail.com>
2025-04-05 15:30:08 +0300

    Create header.php
    ajout header

---
+++ view/components/header.php
@@ -0,0 +1,34 @@
+ <!--/* views/components/header.php */ -->
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bibliothèque Universitaire</title>
+
+  <link rel="stylesheet" href="../css/bootstrap.min.css">
+<script src="../js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="/">Biblio</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="../auth/login.php">Connexion</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../auth/register.php">Inscription</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
