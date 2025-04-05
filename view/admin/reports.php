14f3c8aa34a5037473cdb8b1ae29fc62200c9686
Hasina37 <randriamarohasina@gmail.com>
2025-04-05 15:25:29 +0300

    Create reports.php
    ajout reports

---
+++ view/admin/reports.php
@@ -0,0 +1,18 @@
+
+
+<?php include '../components/header.php'; ?>
+<div class="container mt-5">
+  <h2>Rapports d'activités</h2>
+  <ul class="list-group">
+    <li class="list-group-item">
+      <a href="/admin/rapports/emprunts">Rapport des emprunts</a>
+    </li>
+    <li class="list-group-item">
+      <a href="/admin/rapports/reservations">Rapport des réservations</a>
+    </li>
+    <li class="list-group-item">
+      <a href="/admin/rapports/utilisateurs">Rapport des utilisateurs</a>
+    </li>
+  </ul>
+</div>
+<?php include '../components/footer.php'; ?>
